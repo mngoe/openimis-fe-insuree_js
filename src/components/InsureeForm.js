a2b8b23d5b44ddf07085f0882a5dd7c87cc2ec1f
jobil <j.bilongo@ynote.africa>
2023-05-24 09:13:04 +0100

    correct insuree gender choice

--- src/components/InsureeForm.js
+++ src/components/InsureeForm.js
@@ -117,7 +117,7 @@ class InsureeForm extends Component {
     if (!this.state.insuree.lastName) return false;
     if (!this.state.insuree.otherNames) return false;
     if (!this.state.insuree.dob) return false;
-    if (!this.state.insuree.gender) return false;
+    if (!this.state.insuree.gender || !this.state.insuree.gender?.code) return false;
     if (!this.state.insuree.attachments) return false;
     let attachments = [];
     if (!!this.state.insuree.attachments) {
