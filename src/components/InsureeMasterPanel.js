c2f20a854a4a76671757716d12f03ffe85ec0308
jobil <j.bilongo@ynote.africa>
2023-05-25 11:51:22 +0100

    remove beneficiary card

--- src/components/InsureeMasterPanel.js
+++ src/components/InsureeMasterPanel.js
@@ -144,19 +144,6 @@ class InsureeMasterPanel extends FormPanel {
                       onChange={(v) => this.updateAttribute("marital", v)}
                     />
                   </Grid>
-                  <Grid item xs={3} className={classes.item}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          color="primary"
-                          checked={!!edited && !!edited.cardIssued}
-                          disabled={readOnly}
-                          onChange={(v) => this.updateAttribute("cardIssued", !edited || !edited.cardIssued)}
-                        />
-                      }
-                      label={formatMessage(intl, "insuree", "Insuree.cardIssued")}
-                    />
-                  </Grid>
                   <Grid item xs={12}>
                     <PublishedComponent
                       pubRef="insuree.InsureeAddress"
