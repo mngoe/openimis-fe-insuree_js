db029870108e3948a36a6bfef415c60b01fc2bd0
michellewis <kouamkouammichellewis@gmail.com>
2025-04-17 17:04:34 +0100

    fix(app): Adjustement  on NIN value

--- src/constants.js
+++ src/constants.js
@@ -36,3 +36,5 @@ export const FAMILY_TYPE_POLYGAMY_CODE = "P";
 export const INSUREE_PREFERRED_PAYMENT_METHOD = "PB"
 
 export const WITHOUT_STR = "without"
+export const PASSPORT_MIN_LENGTH = 7
+export const PASSPORT_MAX_LENGTH = 8
