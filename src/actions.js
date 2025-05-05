cafa7f781e33cd81647cc9d0731ec376000a8b16
michellewis <kouamkouammichellewis@gmail.com>
2025-05-05 03:01:27 +0100

    fix(insuree): ajout de la condition pour la récuperation des photos

--- src/actions.js
+++ src/actions.js
@@ -12,7 +12,7 @@ import {
 import { INSUREE_ACTIVE_STRING } from "./constants";
 
 //NOTE: Fetching all INSUREE_FULL_PROJECTION fields except family.
-const FAMILY_HEAD_PROJECTION = (mm) => [
+const FAMILY_HEAD_PROJECTION = (mm, cansearch) => [
   "id",
   "uuid",
   "chfId",
@@ -23,7 +23,7 @@ const FAMILY_HEAD_PROJECTION = (mm) => [
   "age",
   "validityFrom",
   "validityTo",
-  `photo{id,uuid,date,folder,filename,officerId,photo}`,
+  `${!!cansearch ?"" :"photo{id,uuid,date,folder,filename,officerId,photo}"}`,
   "gender{code, gender}",
   "education{id}",
   "profession{id}",
@@ -174,7 +174,7 @@ export function fetchFamilySummaries(mm, filters) {
     "parent{id}",
     "validityFrom",
     "validityTo",
-    `headInsuree{${FAMILY_HEAD_PROJECTION(mm).join(",")}}`,
+    `headInsuree{${FAMILY_HEAD_PROJECTION(mm, true).join(",")}}`,
     "location" + mm.getProjection("location.Location.FlatProjection"),
     "clientMutationId",
   ];
@@ -199,7 +199,7 @@ export function fetchSubFamilySummary(mm, filters) {
     "parent{id}",
     "validityFrom",
     "validityTo",
-    `headInsuree{${FAMILY_HEAD_PROJECTION(mm).join(",")}}`,
+    `headInsuree{${FAMILY_HEAD_PROJECTION(mm, true).join(",")}}`,
     "location" + mm.getProjection("location.Location.FlatProjection"),
     "clientMutationId",
   ];
