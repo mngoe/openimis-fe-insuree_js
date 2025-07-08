ae9d92700c808fc9c372fa1df08a4593bc858d80
Iandraina-etech <a.rabarisoa@etechconsulting-mg.com>
2025-07-08 11:48:09 +0300

    Update HandicapNonPicker.js

--- src/pickers/HandicapNonPicker.js
+++ src/pickers/HandicapNonPicker.js
@@ -63,7 +63,6 @@ class HandicapNonPicker extends Component {
   }
 }
 
-console.log("HandicapNonPicker loaded"+this.props.handicapNonOptions);
 
 const mapStateToProps = (state) => ({
   handicapNonOptions: state.insuree.handicapNonOptions,
