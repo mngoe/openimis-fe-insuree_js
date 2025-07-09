1170a007d4403bfd77995107bd8c7f87c47ced08
vaneck237 <vaneckbilongo@gmail.com>
2025-07-09 15:39:00 +0100

    add console log

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -27,6 +27,10 @@ export const isValidInsuree = (insuree, modulesManager) => {
   const passportMaxLength = modulesManager.getConf("fe-insuree", "passportMaxLength", PASSPORT_MAX_LENGTH);
 
   const insureeChildId = modulesManager.getConf("fe-insuree", "insureeForm.insureeChildId", 4);
+  console.log("passportMinLength: ", passportMinLength);
+  console.log("passportMinLength: ", passportMinLength);
+  console.log("isInsureeStatusRequired: ", isInsureeStatusRequired);
+  console.log("isPhoneNumberMandatory: ", isPhoneNumberMandatory)
   if (isInsureeFirstServicePointRequired && !insuree.healthFacility) return false;
   if (insuree.validityTo) return false;
   // if (!insuree.chfId) return false;
