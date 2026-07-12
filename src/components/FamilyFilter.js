389ebf74a5999b11047d53e390ddb871e8a2bef0
Paul Sandjong <princesandjong777@gmail.com>
2026-07-12 23:35:22 +0100

    feat(insuree): set up filter on insuree affiliation status

--- src/components/FamilyFilter.js
+++ src/components/FamilyFilter.js
@@ -68,6 +68,17 @@ class FamilyFilter extends Component {
     this.props.onChangeFilters(filters);
   };
 
+  _onChangeStatus = (statusValue) => {
+    this.setState({ status: statusValue });
+    this.props.onChangeFilters([
+      {
+        id: "status",
+        value: statusValue,
+        filter: statusValue ? `affiliationType: "${statusValue}"` : null,
+      },
+    ]);
+  };
+
   renderLastNameField = (anchor, classes) => (
     <ControlledField
       module="insuree"
@@ -310,6 +321,8 @@ class FamilyFilter extends Component {
 
   render() {
     const { intl, classes, filters, onChangeFilters, filterPaneContributionsKey } = this.props;
+    const { status } = this.state;
+
     return (
       <Grid container className={classes.form}>
         <ControlledField
@@ -329,6 +342,22 @@ class FamilyFilter extends Component {
         />
         {this.familyHeadFilter()}
         {this.filterFamiliesOnMembers && this.familyMemberFilter()}
+        
+        <ControlledField
+          module="insuree"
+          id="FamilyFilter.status"
+          field={
+            <Grid item xs={2} className={classes.item}>
+              <PublishedComponent
+                pubRef="insuree.InsureeStatusPicker"
+                withNull={true}
+                value={status}
+                onChange={this._onChangeStatus}
+              />
+            </Grid>
+          }
+        />
+        
         <ControlledField
           module="insuree"
           id="FamilyFilter.poverty"
